40b2c3ba510293489bad63026e3fa9bb3469c227
Patryk Młynarek <p.mlynarek@sunscrapers.com>
2020-04-14 23:23:20 +0200

    Initial setup for project.

---
+++ app/entrypoints/test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+while ! nc -z db 5432; do echo "Waiting for db:5432"; sleep 5; done
+while ! echo 'SELECT 1' | PGPASSWORD=$POSTGRES_PASSWORD psql --host $POSTGRES_HOST --user $POSTGRES_USER $POSTGRES_DB; do echo "Waiting for DB"; sleep 5; done
+
+python manage.py test --settings=settings.testing
---
+++ app/settings/staging.py
@@ -0,0 +1,5 @@
+from .dev import *  # NOQA
+
+logger = logging.getLogger(__name__)  # NOQA
+logger.setLevel(logging.DEBUG)  # NOQA
+logger.debug("loading settings staging.py")
